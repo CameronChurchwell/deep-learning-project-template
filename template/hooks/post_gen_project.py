2c45531d5438965f8c437bdd41b18ec06b1f5a35
Cameron Churchwell <cameronchurchwell@icloud.com>
2025-04-18 11:48:44 -0500

    Move to cookiecutter

---
+++ template/hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+import shutil
+
+source_dir = Path(os.getcwd())
+target_dir = source_dir.parent
+
+temp_source_dir = source_dir.parent / 'tmp'
+
+shutil.move(source_dir, temp_source_dir)
+
+for p in temp_source_dir.iterdir():
+    shutil.move(p, target_dir / p.name)
