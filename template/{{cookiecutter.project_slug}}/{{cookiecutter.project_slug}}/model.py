36c6f1583bbe82055717b743e1044b8b8fe4259d
Cameron Churchwell <cameronchurchwell@icloud.com>
2026-03-23 03:41:17 +0000

    remove audio loading from model.py

--- template/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/model.py
+++ template/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/model.py
@@ -10,21 +10,6 @@ import {{cookiecutter.project_slug}}
 ###############################################################################
 
 
-def audio(file):
-    """Load audio from disk"""
-    path = Path(file)
-    if path.suffix.lower() == '.mp3':
-        try:
-            audio, sample_rate = torchaudio.load(path, format='mp3')
-        except RuntimeError:
-            raise RuntimeError(
-                'Failed to load mp3 file, make sure ffmpeg<=4.3 is installed')
-    else:
-        audio, sample_rate = torchaudio.load(file)
-
-    # Maybe resample
-    return {{cookiecutter.project_slug}}.resample(audio, sample_rate)
-
 class Model(torch.nn.Module):
     """Model definition"""
 
