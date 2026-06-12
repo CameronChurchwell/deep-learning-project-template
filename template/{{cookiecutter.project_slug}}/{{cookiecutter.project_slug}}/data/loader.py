1495051fc9d80bf9c7138428ab9bcaead311d427
Cameron Churchwell <cameronchurchwell@icloud.com>
2026-06-12 15:07:01 -0400

    add features argument to dataloader

--- template/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/data/loader.py
+++ template/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/data/loader.py
@@ -3,10 +3,10 @@ import torch
 import {{cookiecutter.project_slug}}
 
 
-def loader(datasets, partition, gpu=None):
+def loader(datasets, partition, features=[], gpu=None):
     """Retrieve a data loader"""
     return torch.utils.data.DataLoader(
-        dataset={{cookiecutter.project_slug}}.data.Dataset(datasets, partition),
+        dataset={{cookiecutter.project_slug}}.data.Dataset(datasets, partition, features=features),
         batch_size={{cookiecutter.project_slug}}.BATCH_SIZE,
         shuffle=partition == 'train',
         num_workers={{cookiecutter.project_slug}}.NUM_WORKERS,
